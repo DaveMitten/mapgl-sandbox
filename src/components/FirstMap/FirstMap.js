ee6b644d38eedca69877edf1abc3005759ce4ff4
David James Mitten <davidmitten88@gmail.com>
2019-09-25 17:59:40 +0100

    Different attempts at getting the animation to work, it does animate up, but unfortunately the loops is inifinite in some way, going to try a manual loop rather that setinterval

--- src/components/FirstMap/FirstMap.js
+++ src/components/FirstMap/FirstMap.js
@@ -32,6 +32,7 @@ class FirstMap extends Component {
 			locationData: "",
 			year: 95,
 			elevationScale: elevationScale.min,
+			newElevationScale: "",
 			yearArr: []
 		};
 
@@ -50,22 +51,66 @@ class FirstMap extends Component {
 		});
 		this._layerRendering();
 		this._animate();
-		// this.cycleYearData();
 	}
+
+
+	incrementScale = () => {
+		const newElevationScale = Math.ceil(this.findNewHeight());
+		this.setState({ newElevationScale });
+		if (this.state.elevationScale >= this.state.newElevationScale) {
+			console.log('clearing interval')
+			clearInterval(this.incrementScale);
+			return '';
+			// return this.cancelInterval();
+		} else {
+			console.log('fired else')
+			return this.setState({
+				elevationScale: this.state.elevationScale + 1
+			});
+		}
+	};
+
+	startInterval = () => {
+		console.log("startinterval fires");
+		return setInterval(this.incrementScale, 10);
+	};
+	cancelInterval = () => {
+		console.log("cancel interval fires");
+		if (this.state.elevationScale >= this.state.newElevationScale){return;}
+		return clearInterval(this.startInterval);
+	};
+
+	findNewHeight = () => {
+		return (
+			this.state.locationData &&
+			this.state.locationData.map(i => i.year[this.state.year])[0] / 5000
+		);
+	};
+
+	yearOnChange = e => {
+		const theCurrentYear = e.currentTarget.value;
+		this.setState({
+			year: theCurrentYear
+		});
+		this.startInterval();
+		// instead of trying to use the setInterval, maybe use the time out and put it into a loop, this way you can control more easily when it is called, try a callback for example,
+		// then infinite loops might be out the question.
+	};
+
 	// deprecated method, looking at componentGetDerivedStateFromProps
 	// need to wrap thi/turn it into a highger order component so i can use the outer layer to require the data and then pass it down as props.
 	// little unsure if this is correct as it will depend on state unless i initialise the state with props passed down from the wrapper, but then that argues whether i would need to
 	// do the wrapper in the first place as i could just start the state with data being passed to it already
-	UNSAFE_componentWillReceiveProps(nextProps) {
-		// if new data is passed through it will initiate the re-redndering of the data and the animation based on the elevation scale, which currently animates correctly upon load
-		if (
-			nextProps.data &&
-			this.props.data &&
-			nextProps.data.length !== this.props.data.length
-		) {
-			this._animate();
-		}
-	}
+	// UNSAFE_componentWillReceiveProps(nextProps) {
+	// 	// if new data is passed through it will initiate the re-redndering of the data and the animation based on the elevation scale, which currently animates correctly upon load
+	// 	if (
+	// 		nextProps.data &&
+	// 		this.props.data &&
+	// 		nextProps.data.length !== this.props.data.length
+	// 	) {
+	// 		this._animate();
+	// 	}
+	// }
 	//   {---------------------- NEW METHOD FOR componentWillReceiveProps -----------------------------}
 	// static getDerivedStateFromProps(nextProps, prevState) {
 	// 	if (nextProps.total !== prevState.total) {
@@ -78,14 +123,10 @@ class FirstMap extends Component {
 	// Would be good in the fianl versio nto extract these to helper functions/animations to clean up the code{ -------_-_-_-___--_-_-_---_-________-____--__-___-_-____---
 	_animate() {
 		this._stopAnimate();
-
-		// wait 7 secs to start animation so that all data are loaded
+		// wait 6 secs to start animation so that all data are loaded
 		this.startAnimationTimer = window.setTimeout(this._startAnimate, 6000);
 	}
 
-	// _startAnimate() {
-	// 	this.intervalTimer = window.setInterval(this._animateHeight, 10);
-	// }
 	_startAnimate() {
 		this.intervalTimer = window.setInterval(this._animateHeight, 10);
 	}
@@ -103,7 +144,9 @@ class FirstMap extends Component {
 		}
 	}
 	// ---------------------------------________------____---------___---___---_________----____-_-_-_-_-_-_--_-_-_-_ }
+
 	_layerRendering = () => {
+		let newObj = null;
 		const newobj =
 			this.state.locationData &&
 			this.state.locationData.map(i => {
@@ -129,28 +172,75 @@ class FirstMap extends Component {
 					return local;
 				},
 				getFillColor: [223, 25, 149], // fluorescent pink
-				getElevation: d => this.state.locationData && Number(d.price / 500),
-				transitions: {
-					getElevation: {
-						duration: 3000,
-						// easing: easeCubicInOut,
-						enter: value => [value[0], value[1], value[2], 0]
-					}
+				getElevation: d => Number(d.price / 500),
+				updateTriggers: {
+					getElevation: newObj
 				}
 			})
 		];
 	};
 
-	yearOnChange = e => {
-		e.stopPropagation();
-		e.nativeEvent.stopImmediatePropagation();
-		this.setState({
-			year: e.currentTarget.value,
-			// elevationScale: this.state.elevationScale - 30
-		});
-		// this._stopAnimate();
-		// this._startAnimate();
-	};
+	// incrementScale = () =>
+	// 	this.setState({ elevationScale: this.state.elevationScale + 1 });
+	// startInterval = () => window.setInterval(this.incrementScale(), 10);
+	// cancelInterval = () => window.clearTimeout(this.startInterval);
+
+	// yearOnChange = e => {
+	// 	const newMax = Object.values(this.state.locationData[0].year)[0] / 100000;
+	// 	const currentYear = e.currentTarget.value;
+	// 	console.log("newMax", newMax, "currentYear", currentYear);
+	// 	const startInterval = () => {
+	// 	  this.startInterval = setInterval(incrementScale, 1000);
+	// 	};
+	// 	const cancelInterval = () => clearInterval(this.startInterval);
+	// 	const incrementScale = () => {
+	// 	  console.log("Starting timer");
+	// 	  console.log(this.state.elevationScale, newMax);
+	// 	  if (
+	// 		this.state.elevationScale >
+	// 		Object.values(this.state.locationData[0].year)[0] / 100000
+	// 	  ) {
+	// 		cancelInterval();
+	// 	  } else {
+	// 		console.log("Inside elevation scale");
+	// 		this.setState({
+	// 		  year: currentYear,
+	// 		  elevationScale: this.state.elevationScale + 1
+	// 		});
+	// 	  }
+	// 	};
+	// 	startInterval();
+	//   };
+
+	// yearOnChange = e => {
+	// 	const newMax =
+	// 		Number.parseInt(Object.values(this.state.locationData[0].year)[0]) /
+	// 		100000;
+	// 	const currentYear = e.currentTarget.value;
+	// 	console.log("newMax", newMax, "currentYear", currentYear);
+	// 	const startInterval = () => {
+	// 		this.startInterval = setInterval(incrementScale, 1000);
+	// 	};
+	// 	const cancelInterval = () => clearInterval(this.startInterval);
+	// 	const incrementScale = () => {
+	// 		console.log("Starting timer");
+	// 		console.log(this.state.elevationScale, newMax);
+	// 		if (
+	// 			this.state.elevationScale >
+	// 			Number.parseInt(Object.values(this.state.locationData[0].year)[0]) /
+	// 				100000
+	// 		) {
+	// 			cancelInterval();
+	// 		} else {
+	// 			console.log("Inside elevation scale");
+	// 			this.setState({
+	// 				year: currentYear,
+	// 				elevationScale: this.state.elevationScale + 1
+	// 			});
+	// 		}
+	// 	};
+	// 	startInterval();
+	// };
 
 	sortYears = () =>
 		dataSet.map(i => {
@@ -179,15 +269,15 @@ class FirstMap extends Component {
 			// 	this.setState({
 			// 		elevationScale: this.state.elevationScale - 40
 			// 	});
-			// 	this._stopAnimate();
-			// 	this._startAnimate();
+			this._stopAnimate();
+			this._startAnimate();
 			// };
 			setTimeout(() => {
 				i++;
 				if (i < this.state.yearArr.length) {
 					this.setState(state => ({
 						year: state.yearArr[i].slice(2),
-						elevationScale: state.elevationScale - 30
+						elevationScale: state.elevationScale - 20
 					}));
 					myLoop();
 				}
@@ -203,10 +293,6 @@ class FirstMap extends Component {
 	// 	console.log("result", result);
 	// 	return result;
 	// };
-
-	findNewHeight = () => {
-		return this.state.locationData.map(i => i.year[this.state.year])[0];
-	};
 
 	render() {
 		return (
